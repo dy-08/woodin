3cd28e837cf9e0a1edc2243a275755a827177083
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-08-09 03:18:43 +0900

    feat: scroll 이벤트 기반으로 section01 진입 시 클래스 적용 처리

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
-const section02 = document.getElementById('section02');
+/**
+ * 마우스휠 이벤트로 섹션 이동 구현
+ * Last updated: 2025‑08‑09
+ */
 
+const section02 = document.getElementById('section02');
 let to = 0;
 
 window.addEventListener('wheel', (event) => {
@@ -14,3 +18,22 @@ window.addEventListener('wheel', (event) => {
   console.log('type: ', typeof isPN);
   window.scrollTo({ top: to, behavior: 'smooth' });
 });
+
+/**
+ * 마우스 스크롤 이벤트를 이용해 초기이벤트 설정
+ * Last updated: 2025‑08‑09
+ */
+function scrollEvent() {
+  let wsy = window.scrollY;
+  console.log(wsy);
+  const min = document.getElementById('section01').offsetTop;
+  console.log(min);
+  if (wsy >= min - 82 && wsy < section02.offsetTop - 82) {
+    console.log(section02.offsetTop);
+    document.getElementById('section01').classList.add('active');
+  } else {
+    document.getElementById('section01').classList.remove('active');
+  }
+}
+scrollEvent();
+window.addEventListener('scroll', scrollEvent);

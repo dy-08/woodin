edc5b96f52994914d43912ea1ade2833b03abb76
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-08-11 01:22:35 +0900

    feat: 헤더 섹션06 다크스타일 적용 및 섹션04 다크스타일 충돌 해결

--- src/main.js
+++ src/main.js
@@ -25,10 +25,14 @@ window.addEventListener('wheel', (event) => {
  * 헤더: 섹션04에서 다크 스타일(배경/글자/로고) 전환
  * 섹션: 섹션04 높이 91.5vh-40px → 100vh로 전환 및 트랜지션 추가
  * Last updated: 2025‑08‑10
+ * 헤더: 섹션06에서 다크 스타일(배경/글자/로고) 전환
+ * 섹션: 섹션06 wrap의 높이를 100vh으로 전환
+ * 에러: 스크롤 이벤트시 변경되는 헤더 다크 스타일부분이 겹쳐 else의 영향을 받음 (섹션04 작동안함)
+ * 해결: 공통 스타일을 요구하는 범위는 같이 묶어줌 ✅
+ * Last updated: 2025‑08‑10
  */
 function scrollEvent() {
   let wsy = window.scrollY;
-  console.log(wsy);
   const min = document.getElementById('section01').offsetTop;
   // console.log(min);
   if (wsy >= min - 82 && wsy < section02.offsetTop - 82) {
@@ -38,23 +42,40 @@ function scrollEvent() {
     document.getElementById('section01').classList.remove('active');
   }
 
-  const item04 = document.getElementById('section04').offsetTop;
-  const item05 = document.getElementById('section05').offsetTop;
+  const s04 = document.getElementById('section04').offsetTop;
+  const inS04 = wsy >= s04 - 82 && wsy < 3503;
   const header = document.getElementById('head');
   const headerImgLogo = document.querySelector('.header__logo');
   const naviItem = document.querySelectorAll('.navi__item');
-
-  if (wsy >= item04 - 82 && wsy < 3503) {
-    // wsy < item05 - 82 = 3504
+  if (wsy >= s04 - 82 && wsy < 3503) {
     header.style.backgroundColor = '#000000';
-    headerImgLogo.src = '../assets/images/common/logo2.png';
-    naviItem.forEach((item) => (item.style.color = '#fff'));
     document.getElementById('section04').style.height = '100vh';
   } else {
     header.style.backgroundColor = 'transparent';
-    headerImgLogo.src = '../assets/images/common/logo.png';
-    naviItem.forEach((item) => (item.style.color = 'rgb(85, 85, 85)'));
     document.getElementById('section04').style.height = 'calc(91.5vh - 40px)';
+  }
+
+  const s06 = document.getElementById('s06_wrap').offsetTop;
+  const inS06 = wsy > s06 && wsy < 5256;
+  if (inS04 || inS06) {
+    headerImgLogo.src = '../assets/images/common/logo2.png'; //✅
+    naviItem.forEach((item) => (item.style.color = '#fff')); //✅
+  } else {
+    headerImgLogo.src = '../assets/images/common/logo.png'; //✅
+    naviItem.forEach((item) => (item.style.color = 'rgb(85, 85, 85)')); //✅
+  }
+  if (wsy > s06 && wsy < 5256) {
+    // wsy의 수치를 기준으로 다음 섹션의 탑 위치를 잡아줘야함
+    document.getElementById('s06_wrap').style.height = '100vh';
+    document.getElementById('s06_wrap').style.backgroundImage =
+      'url(../assets/images/main/s6_brand_bg.jpg';
+    document.getElementById('s06_wrap').style.backgroundPosition = 'center';
+    document.getElementById('s06_wrap').style.filter = 'brightness(90%)';
+  } else {
+    document.getElementById('s06_wrap').style.height = 'auto';
+    document.getElementById('s06_wrap').style.backgroundImage = '';
+    document.getElementById('s06_wrap').style.backgroundPosition = '';
+    document.getElementById('s06_wrap').style.filter = 'brightness(100%)';
   }
 }
 scrollEvent();

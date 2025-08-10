bc0b78a306396f77865e1965053a3baa38ba2cc9
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2025-08-10 15:09:54 +0900

    feat: 섹션04 높이 91.5vh-40px → 100vh로 전환 및 트랜지션 추가"

--- src/main.js
+++ src/main.js
@@ -23,13 +23,14 @@ window.addEventListener('wheel', (event) => {
  * 마우스 스크롤 이벤트를 이용해 초기이벤트 설정
  * Last updated: 2025‑08‑09
  * 헤더: 섹션04에서 다크 스타일(배경/글자/로고) 전환
+ * 섹션: 섹션04 높이 91.5vh-40px → 100vh로 전환 및 트랜지션 추가
  * Last updated: 2025‑08‑10
  */
 function scrollEvent() {
   let wsy = window.scrollY;
   console.log(wsy);
   const min = document.getElementById('section01').offsetTop;
-  console.log(min);
+  // console.log(min);
   if (wsy >= min - 82 && wsy < section02.offsetTop - 82) {
     console.log(section02.offsetTop);
     document.getElementById('section01').classList.add('active');
@@ -43,14 +44,17 @@ function scrollEvent() {
   const headerImgLogo = document.querySelector('.header__logo');
   const naviItem = document.querySelectorAll('.navi__item');
 
-  if (wsy >= item04 - 82 && wsy < item05 - 82) {
+  if (wsy >= item04 - 82 && wsy < 3503) {
+    // wsy < item05 - 82 = 3504
     header.style.backgroundColor = '#000000';
     headerImgLogo.src = '../assets/images/common/logo2.png';
     naviItem.forEach((item) => (item.style.color = '#fff'));
+    document.getElementById('section04').style.height = '100vh';
   } else {
     header.style.backgroundColor = 'transparent';
     headerImgLogo.src = '../assets/images/common/logo.png';
     naviItem.forEach((item) => (item.style.color = 'rgb(85, 85, 85)'));
+    document.getElementById('section04').style.height = 'calc(91.5vh - 40px)';
   }
 }
 scrollEvent();
